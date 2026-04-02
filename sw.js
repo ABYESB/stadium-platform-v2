91cced9015672a6e867a19f71cdf3b376ceebc63
ABYESB <ohthmanbnfakir@gmail.com>
2026-04-02 16:54:01 +0100

    Update cache name to version 1.17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'bouassel-v1.16'; // ارفع النسخة لـ 1.3 لضمان التغيير
+const cacheName = 'bouassel-v1.17'; // ارفع النسخة لـ 1.3 لضمان التغيير
 const assets = [
   '/bouassel/',
   '/bouassel/index.html',
