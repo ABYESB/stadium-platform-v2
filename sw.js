4fb7fb5407b439b180b9c7167121368593035073
ABYESB <ohthmanbnfakir@gmail.com>
2026-04-12 15:06:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'bouassel-v1.88'; // ارفع النسخة لـ 1.3 لضمان التغيير
+const cacheName = 'bouassel-v1.89'; // ارفع النسخة لـ 1.3 لضمان التغيير
 const assets = [
   '/bouassel/',
   '/bouassel/index.html',
