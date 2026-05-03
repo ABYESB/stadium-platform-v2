b2e6d09f49be3ca1d8ef3748cc6af03e118763f0
ABYESB <ohthmanbnfakir@gmail.com>
2026-05-03 02:20:45 +0100

    Bump cache version to malaeb-net-v1.18
    Updated cache version to ensure changes are activated.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'malaeb-net-v1.17'; // تم تحديث النسخة لضمان تفعيل التعديلات
+const cacheName = 'malaeb-net-v1.18'; // تم تحديث النسخة لضمان تفعيل التعديلات
 const assets = [
   './',
   './index.html',
