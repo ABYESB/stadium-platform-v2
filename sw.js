ea1651c9dce76958c55a445c0cfedd29a916b44f
ABYESB <ohthmanbnfakir@gmail.com>
2026-05-04 19:37:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'malaeb-net-v1.29'; // تم تحديث النسخة لضمان تفعيل التعديلات البرمجية
+const cacheName = 'malaeb-net-v1.30'; // تم تحديث النسخة لضمان تفعيل التعديلات البرمجية
 const assets = [
   './',
   './index.html',
