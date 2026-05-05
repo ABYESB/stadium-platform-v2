a5c777758da7e94d0a53cb73fa0dfd63b1e35963
ABYESB <ohthmanbnfakir@gmail.com>
2026-05-05 12:02:11 +0100

    Increment cache version to v1.31
    Updated cache version to ensure changes take effect.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'malaeb-net-v1.30'; // تم تحديث النسخة لضمان تفعيل التعديلات البرمجية
+const cacheName = 'malaeb-net-v1.31'; // تم تحديث النسخة لضمان تفعيل التعديلات البرمجية
 const assets = [
   './',
   './index.html',
