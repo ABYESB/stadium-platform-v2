2bdc569335a1e06e5b5782646278deb5ab772c8b
ABYESB <ohthmanbnfakir@gmail.com>
2026-05-10 22:25:59 +0100

    Update cache version to v1.62
    Updated cache version to ensure changes are activated.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'malaeb-net-v1.61'; // تحديث النسخة لضمان تنشيط التعديلات
+const cacheName = 'malaeb-net-v1.62'; // تحديث النسخة لضمان تنشيط التعديلات
 const assets = [
   './',
   './index.html',
