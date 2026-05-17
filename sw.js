8b3df50cec78199d48af9a43776df48cb3913ca0
ABYESB <ohthmanbnfakir@gmail.com>
2026-05-17 13:08:44 +0100

    Update cache version to malaeb-net-v1.72

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'malaeb-net-v1.71'; // تحديث النسخة لضمان تنشيط التعديلات
+const cacheName = 'malaeb-net-v1.72'; // تحديث النسخة لضمان تنشيط التعديلات
 const assets = [
   './',
   './index.html',
